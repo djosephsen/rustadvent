8ae81a49972a07d792c7ae6896a8a3a74878e297
Dave Josephsen <djosephsen@fastly.net>
2019-12-23 10:54:48 -0600

    day2

---
+++ day_2/target/debug/day_2.d
@@ -0,0 +1,1 @@
+/Users/djosephsen/reps/rust_advent/day_2/target/debug/day_2: /Users/djosephsen/reps/rust_advent/day_2/src/main.rs
---
+++ day_2/target/debug/deps/day_2-6a78a8f01b47f68b.d
@@ -0,0 +1,5 @@
+/Users/djosephsen/reps/rust_advent/day_2/target/debug/deps/day_2-6a78a8f01b47f68b: src/main.rs
+
+/Users/djosephsen/reps/rust_advent/day_2/target/debug/deps/day_2-6a78a8f01b47f68b.d: src/main.rs
+
+src/main.rs:
